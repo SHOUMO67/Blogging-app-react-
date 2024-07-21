459ee5c9fdbca52b32a756bf6a68eaa68248318b
SHOUMO67 <115798128+SHOUMO67@users.noreply.github.com>
2024-07-21 14:06:19 +0530

    firebaseinit.js

--- src/firebaseinit.js
+++ src/firebaseinit.js
@@ -5,14 +5,6 @@ import {getFirestore} from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCMyPbw3s3hNAQFo0WEbQ-bPqYRJFw6_U0",
-  authDomain: "blogging-app-4f3b2.firebaseapp.com",
-  projectId: "blogging-app-4f3b2",
-  storageBucket: "blogging-app-4f3b2.appspot.com",
-  messagingSenderId: "715368593562",
-  appId: "1:715368593562:web:80930f3c4b93787a828723"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
